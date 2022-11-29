33460ecf22897b5bb048963a502300d353486d39
acai <acai@fi.uba.ar>
2022-11-29 20:42:56 -0300

    Fix post transaction body

--- src/handlers/createDepositHandler.js
+++ src/handlers/createDepositHandler.js
@@ -17,11 +17,12 @@ function schema() {
 }
 
 function handler({ contractInteraction, walletService }) {
-  return async function (req) {
-    walletService.getWallet(req.body.senderId).then((the_wallet) => {
-      return contractInteraction.deposit(the_wallet, req.body.amountInEthers);
+  return async function (req, reply) {
+    return walletService.getWallet(req.body.senderId).then(async (the_wallet) => {
+      return reply.code(200).send(await contractInteraction.deposit(the_wallet, req.body.amountInEthers));
     });
   };
 }
 
-module.exports = { schema, handler };
+
+module.exports = { handler, schema };
